35c2fb67e76025cea4b92a916bccc576f0fc7a6a
John Shewchuk <johnshew@users.noreply.github.com>
2018-11-18 10:31:24 -0800

    Cleanup and repository path

--- app.ts
+++ app.ts
@@ -1,6 +1,3 @@
-import * as _debug from 'debug';
-let debug = _debug('app');
 
 import * as httpServer from './server';
-
 export let server = httpServer.create(process.env.port || process.env.PORT || 8080);
